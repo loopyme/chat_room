0c939cd1413a089fd837417d3645c517dc7128a6
loopyme <33747091+loopyme@users.noreply.github.com>
2019-12-13 11:08:25 +0800

    - correct staticmethod - fix raw_data print issue

--- server/server.py
+++ server/server.py
@@ -25,84 +25,62 @@ class Handler:
     def __init__(self, user):
         self.user = user
 
-    # @staticmethod
-    # def getUser(username):
-    #     def getKey(list, value):
-    #         return [k for k,v in d.items() if v == value][0]
-    #     return getKey(Handler.user_pool, username)
-    #
-    # @staticmethod
-    # def delUsername(username):
-    #     try:
-    #         user = Handler.getUser(username)
-    #         Handler.remove_user(user)
-    #     except:
-    #         pass
-
-    @staticmethod
-    def remove_user(user):
+    @classmethod
+    def remove_user(cls,user):
         try:
-            Handler.user_pool.pop(user)
+            cls.user_pool.pop(user)
         except Exception as e:
             log(e, "ERROR")
 
     @staticmethod
     def send_to_users(users, data):
-        raw_data = json.dumps(data).encode()
+        raw_data = json.dumps(data)
         for user in users:
-            user.client_socket.send(raw_data)
+            user.client_socket.send(raw_data.encode())
         log("[send_to_users] " + raw_data, "SUCCESS")
 
     @staticmethod
     def send_to_user(user, data):
-        """向用户列表发送相同的数据包"""
         userList = [k for k, v in Handler.user_pool.items() if v in user]
         Handler.send_to_users(userList, data)
 
+    @staticmethod
+    def private_chat(data):
+        Handler.send_to_user(data["to"], data)
+
+    @staticmethod
+    def group_chat(data):
+        Handler.send_to_users(Handler.user_pool.keys(), data)
+
     def send_back(self, data):
-        """给本用户发送信息包"""
-        raw_data = json.dumps(data).encode()
-        self.user.client_socket.send(raw_data)
+        raw_data = json.dumps(data)
+        self.user.client_socket.send(raw_data.encode())
         log("[send_to_user] " + raw_data, "SUCCESS")
 
     def login(self, data):
-        """处理登录信息包"""
-        # already login
-        if self.user in Handler.user_pool.keys():
+        if self.user in Handler.user_pool.keys():  # already login
             data["status"] = False
             data["info"] = "您已经登录了"
-        # username in use
-        elif data["username"] in Handler.user_pool.values():
+        elif data["username"] in Handler.user_pool.values():  # username in use
             data["status"] = False
             data["info"] = "该用户名已被占用"
-        else:
+        else:  # login success
             data["status"] = True
             Handler.user_pool[self.user] = data["username"]
         self.send_back(data)
+
+    def logout(self, data):
+        log("[log_out] user:{} logout".format(Handler.user_pool[self.user]), "SUCCESS")
+        Handler.user_pool.pop(self.user)
 
     def ping(self):
         self.send_back({"type": "ping"})
 
     def get_list(self, data):
-        """获取在线用户列表"""
         data["list"] = Handler.user_pool.values()
         self.send_back(data)
 
-    def private_chat(self, data):
-        """私聊"""
-        self.send_to_user(data["to"], data)
-
-    def group_chat(self, data):
-        """群聊(公共聊天)"""
-        self.send_to_users(Handler.user_pool.keys(), data)
-
-    def logout(self, data):
-        """登出"""
-        log("[log_out] user:{} logout".format(Handler.user_pool[self.user]), "SUCCESS")
-        Handler.user_pool.pop(self.user)
-
     def __main__(self, data):
-        """处理信息包"""
         switcher = {
             "login": self.login,
             "ping": self.ping,
@@ -121,8 +99,6 @@ class Handler:
 
 
 class ClientThread(threading.Thread):
-    """单个用户线程"""
-
     def __init__(self, addr, client_socket):
         threading.Thread.__init__(self)
         self.user = User(addr, client_socket)
